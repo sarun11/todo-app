cf96b1878188dd68753336f272681f6fd1ca9db6
Sarun Dahal <sarundahal11@gmail.com>
2020-12-23 21:25:14 +0545

    adds static files into the proejct

--- todo_core/views.py
+++ todo_core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import View
 from .models import Task
 from .forms import TaskForm
@@ -13,3 +13,13 @@ class TaskList(View):
         
         context = {'form': form, 'tasks': tasks}
         return render(request, 'todo_core/task.html', context=context)
+    
+    def post(self, request):
+        form = TaskForm(request.POST)
+        
+        if form.is_valid():
+            form.save()
+            return redirect("task_list_url")
+        
+        else:
+            return redirect("task_list_url")
